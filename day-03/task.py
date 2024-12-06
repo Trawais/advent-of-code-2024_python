400afe60d3598af28cf71fa3d9430cf1d2c6ac64
Jan Travnicek <jan.travnicek@bloomreach.com>
2024-12-06 14:14:51 +0100

    dya03

--- day-03/task.py
+++ day-03/task.py
@@ -17,4 +17,12 @@ print(f"First task solution: {sum}")
 
 # Second task
 # First we need to trim the end of the file where there is "don't()" but there is no more "do()" function to enable the calculation again
-print(f"original content length: {len(content)}")
+
+splitted_by_dont = content.split("don't()")
+sum_2 = sum_all_mul_actions(splitted_by_dont[0])
+for i in range(1, len(splitted_by_dont)):
+    if 'do()' in splitted_by_dont[i]:
+        temp = splitted_by_dont[i].split('do()', 1)[1]
+        sum_2 += sum_all_mul_actions(temp)
+
+print(f"Second task: {sum_2}")
